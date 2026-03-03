7180f5fac8c68965a5d6849e1fcb0b1fc44593d6
Vyacheslav Shumkov <49040667+vyacheslavshv@users.noreply.github.com>
2026-03-03 15:37:51 +0500

    Initial commit

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite://data/db.sqlite3")
+
+TORTOISE_ORM = {
+    "connections": {"default": DATABASE_URL},
+    "apps": {
+        "models": {
+            "models": ["models", "aerich.models"],
+            "default_connection": "default",
+        },
+    },
+}
